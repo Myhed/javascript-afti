403e2eedd60c727bbf5bdd9a4e418e3b56b7823f
formateur <you@example.com>
2021-05-03 09:47:06 +0200

    add notion varible javascript

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * @Variable
+ */
+
+var maVariable = ""; // à ne plus utiliser
+const maConstante = 1; // défini une constante
+let maVariableLet = ""; // une variable qui sera amené à être changé durant l'éxécution du code
+
+console.log('nombre entier type: ', typeof 1);
+console.log('nombre à virgule type: ', typeof 1.4);
+
+console.log('chaine de caractère type: ', typeof "hello World!");
+console.log('boolean type: ', typeof false);
+
+console.log('type tableau: ', typeof []);
+console.log('type tableau: ', typeof {});
+
+console.log('calcul: ', 4.5 + 9);
